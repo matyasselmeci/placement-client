7f4dcf492a2e19f1d6af262c6b040c4f5562f827
Matyas Selmeci <mselmeci@wisc.edu>
2026-05-04 14:47:33 -0500

    Remove the 'condor' module
    This was just papering over some of the rough edges in the Condor bindings
    (mostly for submit) which was useful in a demo but out-of-place in a standalone
    client.
    
    Dummy out `__init__.py` since there's not much left anymore.

--- placement/__init__.py
+++ placement/__init__.py
@@ -1,13 +1,1 @@
-from placement.condor import AP, Placement, load_job_description
-
-try:
-    from placement.jupyter import setup
-except ImportError:
-    pass  # widgets not available
-
-__all__ = (
-    "AP",
-    "Placement",
-    "load_job_description",
-    "setup",
-)
+# __init__
